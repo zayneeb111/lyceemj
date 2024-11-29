9a900d14e2d8a02bf1b0439953d0a05ff81d4171
zayneeb111 <164376287+zayneeb111@users.noreply.github.com>
2024-11-29 18:18:35 +0100

    Add files via upload

---
+++ sch.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    slides.forEach((slide, index) => {
+        slide.style.display = (index === slideIndex) ? 'block' : 'none';
+    });
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    const slides = document.querySelectorAll('.slide');
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    showSlides();
+}
+
+
+
+
+
+
+
